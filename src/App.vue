d3d53ad567f6bbc0b7ad5f2ef317df3df1c70b61
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-17 20:25:52 +0200

    change to "dark-theme"

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   import BarChart from './components/BarChart.vue'
 </script>
 
+<style>
+  body { 
+    background-color: #0f0f0f; 
+    color: #f0f0ff;
+  }
+        
+</style>
+
 <template>
   <div id="app">
     <BarChart /> 
