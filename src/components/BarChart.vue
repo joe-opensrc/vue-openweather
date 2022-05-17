c7a9d8020d8d07634b2202d202dd403b97b42d00
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-17 20:25:12 +0200

    a test button

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -8,5 +8,32 @@
 </script>
 
 <template>
-  <div>foo: {{ foo }}</div>  
+  <div class="button-area">
+    <div class="button" @click="foo++">
+      foo: {{ foo }}
+    </div>  
+  </div>
 </template>
+
+<style>
+  .button-area {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .button {
+    min-width: 60px;
+    background-color: #3f3f3f; 
+    margin: 10px;
+    padding: 10px;
+    text-align: center;
+    border-radius: 20px;
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+  }
+</style>
