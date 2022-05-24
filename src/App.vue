9ccdf3aa1418397d6a921ab03f034cc8b83dabd1
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-24 19:06:49 +0200

    cherrypick of d3d53ad
    not sure why; hacked it up anyways :D

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+</script>
+
+<style>
+  body { 
+    background-color: #0f0f0f; 
+    color: #f0f0ff;
+  }
+        
+</style>
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>Welcome to Your Vue.js App</h1>
