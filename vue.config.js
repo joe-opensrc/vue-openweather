660c08288f085d8be40bf1d501a11b06d53e8b01
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-25 19:39:32 +0200

    removes devServer config
    testing on mobile device so want net-local address

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  devServer: {
-    host: '127.0.0.1',
-    port: 8080,
-  }
 })
