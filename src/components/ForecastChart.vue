be005b5132cd0fe358dff1d8ab94cd3ed269a99a
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-25 10:45:57 +0200

    adds Line(chart) component to ForecastChart + defines chartData
    chartData/chart-data is defined as a property

--- src/components/ForecastChart.vue
+++ src/components/ForecastChart.vue
@@ -1,12 +1,19 @@
 <script setup>
 
+  import { defineProps } from 'vue'
   import { Line } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale } from 'chart.js'
+  import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js'
 
-  ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale)
+  ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
+
+  const props = defineProps({
+          chartData: Object
+        }) 
 
 </script> 
 
 <template>
-  <Line :chart-data="[1,2,3,4]" />
+  <h>FOO</h><br />
+  <h>BAR</h><br />
+  <Line :chart-data="props.chartData" />
 </template>
