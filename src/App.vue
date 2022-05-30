2ac69efdde58ef687a4ca8ae2a1a0871a07fd7f3
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-30 18:38:13 +0200

    removes debug line

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@
   const sset  = new Date( fcdata.city.sunset * 1000 ).toTimeString().slice(0, 5)
   const tzone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  console.log( { "x": new Date( cwdata.dt * 1000 ).toTimeString().slice(0,5), "y": cwdata.main.feels_like } )
   const chartData = { 
           datasets: [ 
             { 
