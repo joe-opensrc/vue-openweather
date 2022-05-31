83615a79ba04efb36f2ed4b92e926c3d117c8d94
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-05-31 08:28:22 +0200

    adds function: fetchCurrent
    get current weather data

--- src/composables/openweatherApi.js
+++ src/composables/openweatherApi.js
@@ -7,11 +7,25 @@ const owApi = wrapper(axios, {
   
 })
 
-owApi.__addFilter(/forecast/)
-
 export { owApi }
 
+// no filter, i.e., don't cache
+export async function fetchCurrent(){
+  const resp = await owApi({
+    url: 'https://api.openweathermap.org/data/2.5/weather',
+      method: 'get',
+      params: {
+          appid: process.env.VUE_APP_OWM_APPID, ,
+          units: "metric",
+          lat: 41.232078,
+          lon: 1.795869,
+      }
+  })
+  return resp.data
+}
+
 // add try/catch
+owApi.__addFilter(/forecast/)
 export async function fetchForecast(){
   const resp = await owApi({
     url: 'https://api.openweathermap.org/data/2.5/forecast',
