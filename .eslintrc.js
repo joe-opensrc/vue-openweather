0957ea72c3b1c9734c30e9f6a5cc6821dd954de5
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-06-01 17:26:31 +0200

    modifies eslintrc config; adds no-unused-vars + no-empty
    allows an env var to disable:

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.ESLINT_GIT_DISABLE === 'true' ? 'off' : 'warn',
+    'no-empty': process.env.ESLINT_GIT_DISABLE === 'true' ? 'off' : 'warn'
   }
 }
