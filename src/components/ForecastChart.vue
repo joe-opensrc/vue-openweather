c7dfc7195d2048187efc8ec8a4db2e843e0632a9
Joe OpenSrc <joe-opensrc@protonmail.ch>
2022-06-01 16:09:19 +0200

    makes ForecastChart as simple as possible
    removes buttons + css + and update logic
    simply a container to hold a Line vue-chartjs component

--- src/components/ForecastChart.vue
+++ src/components/ForecastChart.vue
@@ -19,58 +19,11 @@
 
         }) 
 
-  import { fetchForecast } from '../composables/openweatherApi.js'
-  import { ref, watch } from 'vue'
-
-  var data = ref(props.chartData)
-  var crefresh = ref(false)
-
-  watch( crefresh, async function(){
-
-    try {
-        const fcdata = await fetchForecast()
-        const data = fcdata.list.map( f => { return { "x": f.dt_txt, "y": f.main.feels_like  }; } )
-        const city = fcdata.city.name
-        const srise = new Date( fcdata.city.sunrise * 1000 ).toTimeString().slice(0, 5)
-        const sset  = new Date( fcdata.city.sunset * 1000 ).toTimeString().slice(0, 5)
-        console.log( fcdata, data, city, srise, sset )
-    } catch (error) {
-        console.log("Error! Could not reach the API." + error)
-    }
-  
-  })
-
 </script> 
 
 <style scoped>
-  .button-area {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .button {
-    min-width: 60px;
-    background-color: #3f3f3f; 
-    margin: 10px;
-    padding: 10px;
-    text-align: center;
-    border-radius: 14px;
-    -webkit-touch-callout: none; /* iOS Safari */
-      -webkit-user-select: none; /* Safari */
-       -khtml-user-select: none; /* Konqueror HTML */
-         -moz-user-select: none; /* Old versions of Firefox */
-          -ms-user-select: none; /* Internet Explorer/Edge */
-              user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome, Edge, Opera and Firefox */
-  }
- 
 </style>
 
 <template>
-    <div class="button-area">
-      <div class="button" @click="crefresh = !crefresh">
-        foo: {{ crefresh }}
-      </div>  
-    </div>
-    <Line v-show="crefresh" :id="props.chartId" :class="props.chartId" :chart-data="data" :chart-options="props.chartOptions" />
+    <Line :id="props.chartId" :class="props.chartId" :chart-data="props.chartData" :chart-options="props.chartOptions" />
 </template>
