fb6c0252ff31119a31a384d42b1976c049ecd2b5
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-03-01 20:27:51 +0100

    make it actualy work ;)

--- yarnVirt.sh
+++ yarnVirt.sh
@@ -3,14 +3,20 @@
 # Here be dragons!
 # Provided ASIS
 
-pyvirtDir="${1:-~/pyvirts}"
+pyvirtDir="${1:-~/pyvirts/nodeenv}"
+novirtDir="${2:-~/novirts/vueopen}"
 
-# Use virtualenv / venv to install nodeenv:
-python -mvenv --copies "${pyvirtDir}/nodeenv"
+# Use virtualenv / venv to create pyvirtdir install nodeenv:
+python -mvenv --copies "${pyvirtDir}"
 
-# Use nodeenv to install node/yarn:
-source "${pyvirtDir}/nodeenv/bin/activate"
+# use pip to install nodeenv 
+source "${pyvirtDir}/bin/activate"
+pip install nodeenv 
+
+# Use nodeenv to install npm
+nodeenv -n lts "${novirtDir}"
 
 # Use npm to install yarn:
+source "${novirtDir}/bin/activate"
 npm install -g yarn
 
